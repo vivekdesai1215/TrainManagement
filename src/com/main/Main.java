534d9f639e0251df3a6c5e8d2d560c004e410e88
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-03-10 14:25:15 +0530

    UC5 : Preserve Insertion Order of Bogies (LinkedHashSet)

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,11 +1,11 @@
 package com.main;
 import java.util.*;
 
-//Use Case 4 : Using LinkedList to maintain insertion Order
-// In this use case I LinkedList to maintain Insertion order of bogies
-// And also perform LinkedList operations
+//Use Case 5 : Using LinkedHashSet to maintain Insertion Order and avoid Duplicate Insertion
+// In this use case I used LinkedHashSet to insert train bogies
+// LinkedHashSet maintains the Insertion order of Bogies and also avoids Duplicate Insertions
 
-// @version 4.0
+// @version 5.0
 // @author Vivek
 
 
@@ -15,25 +15,15 @@ public class Main {
 		System.out.println("----------------------------------------");
 		System.out.println("-------- Train Management App ----------");
 		System.out.println("----------------------------------------");
-		List<String> train = new LinkedList<>();
+		Set<String> train = new LinkedHashSet<>();
 		
 		train.add("Engine");
 		train.add("Sleeper");
 		train.add("AC");
 		train.add("Cargo");
 		train.add("Guard");
-		System.out.println("Initially Train contains : "+train.toString());
-		
-		System.out.println("\nInsterting 'Pantry Car' at 2nd position");
-		train.add(2,"Pantry Car");
-		System.out.println("Train : "+train);
-		train.removeFirst();
-		train.removeLast();
-		System.out.println("\nAfter removing first and last bogie.");
-		System.out.println("Train contains : "+train);
-		
-		
-		System.out.println("\nUC4 Operations completed successfully !!");
+		System.out.println("Train contains : "+train.toString());
+		System.out.println("\nUC5 Operations completed successfully !!");
 	}
 	
 }
